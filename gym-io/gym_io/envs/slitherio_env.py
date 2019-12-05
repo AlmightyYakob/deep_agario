6f283583edaf4144ccf4b69bdf9dbfa6215c9c83
Jacob Nesbitt <jjnesbitt2@gmail.com>
2019-12-05 18:51:30 -0500

    WIP add mouse actions

--- gym-io/gym_io/envs/slitherio_env.py
+++ gym-io/gym_io/envs/slitherio_env.py
@@ -1,7 +1,11 @@
+import math
+import numpy as np
+
 from gym import Env, spaces
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.expected_conditions import (
@@ -18,11 +22,17 @@ SLITHERIO_INITIAL_LENGTH = 10
 PLAY_BUTTON_CLASSNAME = "nsi"
 PLAY_BUTTON_CLASS_INDEX = 2
 
-LENGTH_CSS_SELECTOR = "div.nsi > span > span:nth-child(2)"
-CANVAS_SELCTOR = "canvas.nsi"
-
 OVERLAY_CSS_SELECTOR = "div.nsi"
 OVERLAY_INDICES = [12, 13, 14, 15, 16, 17]
+
+JS_MOUSE_VAR = "window.mouse"
+
+# Num degrees + the boost action
+DEGREE_GRANULARITY = 360
+NUM_ACTIONS = DEGREE_GRANULARITY + 1
+MOUSE_RADIUS_FRACTION = 0.1
+
+DEFAULT_CHROME_OPTIONS = {"width": 300, "height": 300, "headless": True}
 
 
 class SlitherIOEnv(Env):
@@ -33,27 +43,69 @@ class SlitherIOEnv(Env):
     # observation_space.shape =
 
     # Representing degrees to move mouse, might change
-    action_space = spaces.Discrete(360)
-    action_space.shape = (1, 360)
+    action_space = spaces.Discrete(NUM_ACTIONS)
+    action_space.shape = (1, NUM_ACTIONS)
 
-    def __init__(self):
-        # Initialize selenium, open website but don't click play yet
-        # Apply required CSS to dom elements to hide overlays and stuff
+    def __init__(self, **kwargs):
+        options = {**DEFAULT_CHROME_OPTIONS, **kwargs}
+
+        desired_window_width = options["width"]
+        desired_window_height = options["height"]
+        headless = options["headless"]
 
         chrome_options = Options()
-        # chrome_options.add_argument("--headless")
-        # chrome_options.add_argument("window-size=300,300")
+        chrome_options.add_argument(
+            f"window-size={desired_window_width},{desired_window_height}"
+        )
+
+        if headless:
+            chrome_options.add_argument("--headless")
 
         self.driver = webdriver.Chrome(options=chrome_options)
+        self.actions = ActionChains(self.driver)
         self.wait = WebDriverWait(self.driver, 15)
 
+        # Initialize connection
         self.driver.get(SLITHERIO_URL)
+
+        size = self.driver.get_window_size()
+        self.window_size = (size["width"], size["height"])
+        self.window_center = (self.window_size[0] / 2, self.window_size[1] / 2)
+        self.mouse_radius = MOUSE_RADIUS_FRACTION * min(self.window_size)
+        print("radius = ", self.mouse_radius)
+
+        self.observation_space = spaces.Box(
+            low=0, high=255, shape=(*self.window_size, 1), dtype=np.uint8
+        )
+
+        self.inject_mouse_tracking_script()
+        self.center_mouse()
 
     def reset_game(self):
         self.length = SLITHERIO_INITIAL_LENGTH
         self.playing = False
         self.snake_exists = False
         self.game_ready = False
+
+    def inject_mouse_tracking_script(self):
+        self.driver.execute_script(
+            f"let currentFunc = onmousemove;"
+            f"{JS_MOUSE_VAR} = {{x: 0, y: 0}};"
+            f"onmousemove = function(e){{ currentFunc(e); {JS_MOUSE_VAR}.x = e.clientX; {JS_MOUSE_VAR}.y = e.clientY }};"
+        )
+        self.actions.move_by_offset(1, 1).perform()
+
+    def center_mouse(self):
+        pos = self.get_mouse_pos()
+        while (pos != self.window_center):
+            print(pos)
+            pos = self.get_mouse_pos()
+            offset = (self.window_center[0] - pos[0], self.window_center[1] - pos[1])
+            self.actions.move_by_offset(*offset).perform()
+
+    def get_mouse_pos(self):
+        pos = self.driver.execute_script(f"return {JS_MOUSE_VAR};")
+        return (pos["x"], pos["y"])
 
     def wait_for_existing_values(self):
         if not self.playing:
@@ -94,11 +146,44 @@ class SlitherIOEnv(Env):
         if not self.game_ready:
             self.wait_for_existing_values()
 
-        obs = self.observe()
+        # obs = self.observe()
+        obs = 1
+
         score = self.get_score()
-        print("Score:", score)
+        # print("Score:", score)
 
         # Take action
+        if action == self.action_space.n:
+            # Special action
+            pass
+        else:
+            radians = math.radians(action)
+            print("action = ", action)
+            print("radians = ", radians)
+            print("cos = ", math.cos(radians))
+            print("sin = ", math.sin(radians))
+
+            target = (
+                self.window_center[0] + math.cos(radians) * self.mouse_radius,
+                self.window_center[1] + math.sin(radians) * self.mouse_radius,
+            )
+
+            mouse_pos = self.get_mouse_pos()
+
+            offset = (target[0] - mouse_pos[0], target[1] - mouse_pos[1])
+            offset = (offset[0]/10, offset[1]/10)
+
+            print("WINDOW SIZE:", self.window_size)
+            print("POS:", mouse_pos)
+            print("TARGET:", target)
+            print("OFFSET:", offset)
+            print("MATH:", (offset[0] + mouse_pos[0], offset[1] + mouse_pos[1]))
+            self.actions.move_by_offset(*offset).perform()
+
+            # self.actions.move_by_offset(target[0]/100, target[1]/100).perform()
+            # self.driver.
+            # print(new_mouse_x, new_mouse_y)
+
         # new_score = self.get_score()
         # reward = new_score - score
         # self.length = new_score
--- slither.py
+++ slither.py
@@ -3,7 +3,8 @@ import gym_io # NOQA
 
 
 env = gym.make("slitherio-v0")
+# env = gym.make("slitherio-v0", headless=False)
 obs = env.reset()
 
 while True:
-    res = env.step(0)
+    res = env.step(env.action_space.sample())
