ae82ee6faaeb47d6714a61d7f55635320ebb887b
Your Name <you@example.com>
2019-12-13 00:33:05 +0000

    Add loading of new model

--- test_slither.py
+++ test_slither.py
@@ -1,7 +1,8 @@
 import gym
 import gym_io
 from keras.models import load_model
-from slither import conv_model, CustomProcessor, DQN_MEMORY_SIZE
+from slither import conv_model, LSTMProcessor, DQN_MEMORY_SIZE
+from models import full_combined_conv_lstm_model
 
 from keras.models import Sequential
 from rl.agents.dqn import DQNAgent
@@ -17,10 +18,11 @@ from keras.optimizers import Adam
 
 
 env = gym.make("slitherio-v0", headless=False, width=500, height=500)
-model = conv_model(env)
+# model = conv_model(env)
+model = full_combined_conv_lstm_model(env)
 # model = load_model("current_model.h5")
 print(model.weights)
-model.load_weights("current_model.h5")
+model.load_weights("new_current_model.h5")
 print(model.weights)
 policy = EpsGreedyQPolicy(eps=0.2)
 memory = SequentialMemory(limit=DQN_MEMORY_SIZE, window_length=1)
@@ -31,7 +33,7 @@ dqn = DQNAgent(
     # nb_steps_warmup=DQN_MEMORY_SIZE,
     target_model_update=1e-2,
     policy=policy,
-    processor=CustomProcessor(),
+    processor=LSTMProcessor(),
 )
 dqn.compile(Adam(lr=1e-3), metrics=["mae"])
 
